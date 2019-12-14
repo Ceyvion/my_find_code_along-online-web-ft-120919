5e8f79537c663f47afd782524908115fc887bf38
Ceyvion Biggs <ceyvionb@gmail.com>
2019-12-14 22:06:59 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,13 @@
 require 'pry'
 
 def my_find(collection)
+  index = 0 
+  
+  while index < collection.length 
+    yield(collection[index])
+    return collection[index]
+    end 
+  i += 1 
+end 
 
 end
